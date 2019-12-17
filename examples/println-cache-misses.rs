f86bb13457ef24b53582044729345c2a209bddd2
Jim Blandy <jimb@red-bean.com>
2019-12-16 22:38:58 -0800

    println-cache-misses.rs: New example.

---
+++ examples/println-cache-misses.rs
@@ -0,0 +1,22 @@
+use perf_event::Builder;
+use perf_event::event_kind::{Cache, CacheOp, CacheResult, WhichCache};
+
+fn main() -> std::io::Result<()> {
+    let mut branch_counter = Builder::new()
+        .kind(Cache {
+            which: WhichCache::L1D,
+            operation: CacheOp::READ,
+            result: CacheResult::MISS,
+        })
+        .build()?;
+
+    let vec = (0..=51).collect::<Vec<_>>();
+
+    branch_counter.enable()?;
+    println!("{:?}", vec);
+    branch_counter.disable()?;
+
+    println!("{} branches", branch_counter.read()?);
+
+    Ok(())
+}
