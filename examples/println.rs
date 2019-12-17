af27925cb435b779cb8d4f3a73abd517daab321b
Jim Blandy <jimb@red-bean.com>
2019-12-16 22:32:30 -0800

    examples/println.rs: Update for what we're counting now.

--- examples/println.rs
+++ examples/println.rs
@@ -1,18 +1,18 @@
 use perf_event::Builder;
-use perf_event::event_kind::{EventKind, Hardware};
+use perf_event::event_kind::Hardware;
 
 fn main() -> std::io::Result<()> {
-    let mut cycles = Builder::new()
+    let mut branch_counter = Builder::new()
         .kind(Hardware::BRANCH_INSTRUCTIONS)
         .build()?;
 
     let vec = (0..=51).collect::<Vec<_>>();
 
-    cycles.enable()?;
+    branch_counter.enable()?;
     println!("{:?}", vec);
-    cycles.disable()?;
+    branch_counter.disable()?;
 
-    println!("{} cycles", cycles.read()?);
+    println!("{} branches", branch_counter.read()?);
 
     Ok(())
 }
