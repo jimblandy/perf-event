eca920faa27c245ebef38b11ce4a8824bb30c3ba
Phantomical <phantom@lynches.ca>
2023-04-09 00:01:18 -0700

    Add minimal support for perf-event sampling
    This commit introduces a minimal but mostly complete form of support for
    reading sampled perf_event events emitted by the kernel.
    
    New public types being introduced
    =================================
    - Sampler - A combination of an existing Counter with a memory mapped
      ring buffer. It is constructed from a Counter by calling sampled and
      is used to read Records
    - Record - A reference into the kernel ring buffer for a single record
      that allows accessing the emitted bytes. Upon being dropped, it
      advances the tail pointer within the ring buffer. It also provides
      some convenience methods for getting the referenced bytes in owned or
      contiguous forms.
    - SampleType - This is a set of bitflags which indicate what data the
      kernel should collect when sampling. It is a straightforward binding
      of the kernel PERF_SAMPLE_* enum values.
    
    One thing to note is that this commit does not include any support for
    actually deserializing the records emitted by the kernel into something
    rust code can easily use. This can be added on top of the Sampler and
    Record types in a straightforward, albeit not necessarily easy, fashion.
    I consider it to be out of scope for this specific commit.
    
    New Dependencies
    ================
    This commit introduces two new dependencies and one dev-dependency:
    - memmap2   - for memory mapping the kernel ring buffer
    - nix (dev) - for easy querying of the sysconf SC_PAGESIZE value which
      is needed to write the test case for a MMAP record.
    
    Unsupported Features
    ====================
    This section can also be taken as notes for which features would make
    for a good starting point for future work.
    - Support for any of the various configuration flags needed to make the
      kernel generate any record type beyond PERF_RECORD_MMAP.
    - Support for parsing any of the record types. This may be best done in
      an external crate so it can be shared between the parsing needed for
      perf-event and that needed for parsing perf.data files.
    - Support for reading data out of the aux buffer.
    - Support for reading any of the extra information available within
      memory mapped buffer.

---
+++ perf-event/tests/sampling/mmap.rs
@@ -0,0 +1,70 @@
+use super::Hex;
+use nix::unistd::SysconfVar;
+use perf_event::events::Software;
+use perf_event::Builder;
+use perf_event_open_sys::bindings;
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub struct MmapRecord {
+    pub pid: u32,
+    pub tid: u32,
+    pub addr: u64,
+    pub len: u64,
+    pub pgoff: u64,
+}
+
+impl MmapRecord {
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        let mut buf = [0; std::mem::size_of::<Self>()];
+        buf.copy_from_slice(bytes);
+        unsafe { std::mem::transmute(buf) }
+    }
+}
+
+#[test]
+fn record_executable_mmap() {
+    // Use the pagesize as the mmap size so that the recorded length in the
+    // record and the length we allocate are the same.
+    let pagesize = nix::unistd::sysconf(SysconfVar::PAGE_SIZE)
+        .expect("Unable to get page size")
+        .expect("No page size returned") as usize;
+
+    let mut sampler = Builder::new()
+        .kind(Software::DUMMY)
+        .mmap(true)
+        .build()
+        .expect("Failed to build counter")
+        .sampled(4096)
+        .expect("Failed to build sampler");
+
+    sampler.enable().expect("Failed to enable sampler");
+
+    let mmap = memmap2::MmapOptions::new()
+        .len(pagesize)
+        .map_anon()
+        .expect("Failed to create anonymous memory map");
+
+    // This should cause the sampler to record a MMAP event
+    let mmap = mmap
+        .make_exec()
+        .expect("Failed to transition memory mapping to be executable");
+
+    sampler.disable().expect("Failed to disable sampler");
+
+    let record = sampler
+        .next_record()
+        .expect("Sampler did not record any events");
+
+    assert_eq!(record.ty(), bindings::PERF_RECORD_MMAP);
+
+    let bytes = record.to_contiguous();
+    let record = MmapRecord::from_bytes(&bytes[..std::mem::size_of::<MmapRecord>()]);
+
+    eprintln!("record: {:#?}", record);
+
+    assert_eq!(Hex(record.addr), Hex(mmap.as_ptr() as usize as _));
+    assert_eq!(record.len, mmap.len() as _);
+    assert_eq!(record.pid, nix::unistd::getpid().as_raw() as _);
+    assert_eq!(record.tid, nix::unistd::gettid().as_raw() as _);
+}
