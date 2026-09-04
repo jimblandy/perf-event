013dce164819df8ce435fcf7f2603220d4d6c2f5
Jimmy Brisson <jbrisson@linux.ibm.com>
2026-09-04 15:46:21 -0500

    Support S390X
    arch s390 (no x) in the linux kernel
    
    Signed-off-by: Jimmy Brisson <jbrisson@linux.ibm.com>

--- perf-event-open-sys/regenerate.sh
+++ perf-event-open-sys/regenerate.sh
@@ -124,6 +124,7 @@ echo "$version" > src/version
 
 gen_bindings x86_64
 gen_bindings i686 i386
+gen_bindings s390x s390
 gen_bindings aarch64 arm64
 gen_bindings riscv64gc riscv riscv64
 gen_bindings powerpc64 powerpc
